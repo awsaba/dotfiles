a859237af61ccd71c90b17c38006f7b802de75b9
Antony Saba <awsaba@gmail.com>
2014-03-27 17:09:27 -0600

    Some different colors for fun.

--- bash_prompts/tw-solarized.sh
+++ bash_prompts/tw-solarized.sh
@@ -24,16 +24,16 @@ let promptsize=$(echo -n "--(${usernam}@${hostnam})---(${git_ps1}${PWD})--" \
                  | wc -c | tr -d " ")
 let fillsize=${TERMWIDTH}-${promptsize}
 fill=""
-while [ "$fillsize" -gt "0" ] 
-do 
+while [ "$fillsize" -gt "0" ]
+do
     fill="${fill}-"
-	let fillsize=${fillsize}-1
+    let fillsize=${fillsize}-1
 done
 
 if [ "$fillsize" -lt "0" ]
 then
    let cut=3-${fillsize}
-	newPWD="...$(echo -n $PWD | sed -e "s/\(^.\{$cut\}\)\(.*\)/\2/")"
+   newPWD="...$(echo -n $PWD | sed -e "s/\(^.\{$cut\}\)\(.*\)/\2/")"
 fi
 }
 
@@ -46,6 +46,8 @@ local LIGHT_GRAY="\[\033[0;37m\]"
 local RED="\[\033[0;31m\]"
 local WHITE="\[\033[1;37m\]"
 local NO_COLOUR="\[\033[0m\]"
+local MAGENTA="\e[35m"
+local ORANGE="\e[38;5;172m"
 
 local LIGHT_BLUE="\[\033[0;34m\]"
 local YELLOW="\[\033[0;33m\]"
@@ -70,10 +72,10 @@ esac
 
 PS1="$TITLEBAR\
 \n$GREEN-$LIGHT_BLUE-(\
-$USER_COLOR\$usernam$LIGHT_BLUE@$GREEN\$hostnam$WHITE\\
+$USER_COLOR\$usernam$LIGHT_BLUE@$MAGENTA\$hostnam$WHITE\\
 ${LIGHT_BLUE})-${GREEN}-\${fill}${LIGHT_BLUE}-(\
 \$(__git_ps1 \"$WHITE%s$LIGHT_BLUE:\")\
-$YELLOW\${newPWD}\
+$ORANGE\${newPWD}\
 $LIGHT_BLUE)-$GREEN-\
 \n\
 $GREEN-$LIGHT_BLUE-(\
@@ -82,7 +84,7 @@ $LIGHT_BLUE:\
 \[\033[\$(( (\$retstat == 0 ) ? 37 : 31 ))m\]\$retstat\
 $LIGHT_BLUE:$WHITE\$$LIGHT_BLUE)-\
 $GREEN-\
-$NO_COLOUR " 
+$NO_COLOUR "
 
 #PS1="$TITLEBAR\
 #$YELLOW-$LIGHT_BLUE-(\
@@ -95,7 +97,7 @@ $NO_COLOUR "
 #$YELLOW\$(date +%H%M)$LIGHT_BLUE:$YELLOW\$(date \"+%a,%d %b %y\")\
 #$LIGHT_BLUE:$WHITE\$$LIGHT_BLUE)-\
 #$YELLOW-\
-#$NO_COLOUR " 
+#$NO_COLOUR "
 
 PS2="$LIGHT_BLUE-$YELLOW-$YELLOW-$NO_COLOUR "
 
